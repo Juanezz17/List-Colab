43fd02e87f053c8633e22b020f4efe896b8822d7
Juanezz17 <juanvarela1730@gmail.com>
2025-09-20 11:10:35 -0500

    subiendo github actions

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: '/List-Colab/',
   plugins: [react()],
   test: {
     environment: "jsdom", 
